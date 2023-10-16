ef996afaadfd9f0f94d0d2ba372842a0796a72d7
qvixnh <nguyenvinh22012002@gmail.com>
2023-10-16 16:23:38 +0700

    section  2- create category view

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -14,8 +14,12 @@ namespace BulkyWeb.Controllers
         }
         public IActionResult Index()
         {
-            List<Category> objCategoryList =_db.Categories.ToList();
+            List<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
+        }
+        public IActionResult Create()
+        {
+            return View();
         }
     }
 }
